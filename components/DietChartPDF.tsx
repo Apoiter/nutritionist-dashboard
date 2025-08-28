092c18f1a2d4b03c250a34c54d1fdcce0f62632a
E22CSEU0904 <you@example.com>
2025-08-28 15:45:07 +0530

    Finalize PDF header design

--- components/DietChartPDF.tsx
+++ components/DietChartPDF.tsx
@@ -90,6 +90,7 @@ export default function DietChartPDF({ patientName, dietChart, letterheadSetting
           </p>
           <p style={{ margin: '4px 0 0' }}>
             Certified Diet & Fitness Coach<br />
+            Certified Nutrition & Fitness Coach (INFS) <br />
             Certified Fitness/Wellness Coach (Medvarsity & Apollo Life)<br />
             Fellowship in Applied Nutrition<br />
             Fellowship in Clinical Nutrition (Medvarsity & Apollo Hospitals Education & Research Foundation)<br />
